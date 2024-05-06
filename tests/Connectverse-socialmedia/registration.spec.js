5c53db30eac1351b1d2785db6a22c169cff99a5c
Yashika.U <97971705+Yashika24-U@users.noreply.github.com>
2024-05-06 22:03:38 +0530

    update testcases and code cleanup

--- tests/Connectverse-socialmedia/registration.spec.js
+++ tests/Connectverse-socialmedia/registration.spec.js
@@ -50,55 +50,6 @@ test('successtest', async ({ page }) => {
 
 
 
-// test('Imagefiletypecheck', async ({ page }) => {
-//   await page.goto('https://myconnectverse.netlify.app/');
-//   await page.getByText('Don\'t have an account ? Sign').click();
-//   await page.getByLabel('First Name').click();
-//   await page.getByLabel('First Name').fill('test');
-//   await page.getByLabel('Last Name').click();
-//   await page.getByLabel('Last Name').fill('user3');
-//   await page.getByLabel('Location').click();
-//   await page.getByLabel('Location').press('CapsLock');
-//   await page.getByLabel('Location').fill('B');
-//   await page.getByLabel('Location').press('CapsLock');
-//   await page.getByLabel('Location').fill('Bangalore');
-//   await page.getByLabel('Occupation').click();
-//   await page.getByLabel('Occupation').press('CapsLock');
-//   await page.getByLabel('Occupation').fill('P');
-//   await page.getByLabel('Occupation').press('CapsLock');
-//   await page.getByLabel('Occupation').fill('Photographer');
-//   await page.getByText('Add Picture Here').click();
-//   await page.getByLabel('Email').click();
-//   await page.getByLabel('Email').fill('testuser3@gmail.com');
-//   await page.getByLabel('Password').click();
-//   await page.getByLabel('Password').fill('testuser3@gmail.com');
-//   await page.getByRole('button', { name: 'REGISTER' }).click();
-// });
 
-
-// test('profilepictureabsence', async ({ page }) => {
-//   await page.goto('https://myconnectverse.netlify.app/');
-//   await page.getByText('Don\'t have an account ? Sign').click();
-//   await page.getByLabel('First Name').click();
-//   await page.getByLabel('First Name').fill('test');
-//   await page.getByLabel('Last Name').click();
-//   await page.getByLabel('Last Name').fill('user4');
-//   await page.getByLabel('Location').click();
-//   await page.getByLabel('Location').fill('delhi');
-//   await page.getByLabel('Occupation').click();
-//   await page.getByLabel('Occupation').fill('software engineer');
-//   await page.getByLabel('Email').click();
-//   await page.getByLabel('Email').fill('testuser4@gmail.com');
-//   await page.getByLabel('Password').click();
-//   await page.getByLabel('Password').fill('testuser4@gmail.com');
-//   await page.getByRole('button', { name: 'REGISTER' }).click();
-// });
-
-
-// test('emptyregistration', async ({ page }) => {
-//   await page.goto('https://myconnectverse.netlify.app/');
-//   await page.getByText('Don\'t have an account ? Sign').click();
-//   await page.getByRole('button', { name: 'REGISTER' }).click();
-// });
 
 
